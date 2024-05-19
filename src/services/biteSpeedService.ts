81c350b405c2a77d71faf5d5fe219781096c4a77
vishal.jagamani@portqii.com <vishal.jagamani@portqii.com>
2024-05-19 09:51:38 +0530

    adding new contact if not exists

--- src/services/biteSpeedService.ts
+++ src/services/biteSpeedService.ts
@@ -1,4 +1,4 @@
-import { getContactByEmailOrPhoneNumber } from '../utils/supabase.js';
+import { getContactByColumn, insertNewContactInDB } from '../utils/supabase.js';
 
 interface identifyPayload {
     email?: string;
@@ -8,13 +8,52 @@ interface identifyPayload {
 export const identify = async (details: identifyPayload) => {
     try {
         const { email, phoneNumber } = details;
-        if (!(email || phoneNumber)) return { status: false, message: 'Contact details not found' };
+        if (!(email && phoneNumber)) return { status: false, message: 'Contact details not found' };
         else {
-            const result = await getContactByEmailOrPhoneNumber(email, phoneNumber);
-            return result;
+            const contactByEmail = await getContactByColumn('email', email || '');
+            const contactByPhone = await getContactByColumn('phoneNumber', phoneNumber || '');
+            if (!(contactByEmail && contactByPhone && contactByEmail?.length && contactByPhone?.length)) {
+                // Create new contact
+                return await insertNewContact(email, phoneNumber);
+            } else {
+                // Existing contact
+                return { status: false, message: 'Existing contact' };
+            }
         }
     } catch (err) {
         console.log('Error in biteSpeedService.identify service', err);
+        return { status: false, message: 'Error in service' };
+    }
+};
+
+// Function to insert new contact details in db
+const insertNewContact = async (email?: string, phoneNumber?: string) => {
+    try {
+        if (!(email && phoneNumber)) return { status: false, message: 'Contact details not found' };
+        else {
+            const json = {
+                email: email || null,
+                phoneNumber: phoneNumber || null,
+                linkPrecedence: 'primary',
+                createdAt: new Date().toISOString().replace('T', ' '),
+                updatedAt: new Date().toISOString().replace('T', ' '),
+            };
+            const insertContact = await insertNewContactInDB(json);
+            if (insertContact && insertContact?.length) {
+                return {
+                    contact: {
+                        primaryContactId: null,
+                        emails: [email],
+                        phoneNumbers: [phoneNumber],
+                        secondaryContactIds: [],
+                    },
+                };
+            } else {
+                return { status: false, message: 'New contact not inserted' };
+            }
+        }
+    } catch (err) {
+        console.log('Error in biteSpeedService.upsertNewContact service', err);
         return { status: false, message: 'Error in service' };
     }
 };
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,11 +1,21 @@
 import { supabase } from '../config/dbConnection.js';
 
-export const getContactByEmailOrPhoneNumber = async (email?: string, phoneNumber?: string) => {
+export const getContactByColumn = async (columnName: string, columnValue: string) => {
     try {
-        const { data: Contact } = await supabase.from('Contact').select('*').eq('email', email);
+        const { data: Contact } = await supabase.from('Contact').select('*').eq(columnName, columnValue);
         return Contact;
     } catch (err) {
         console.log('Error in getContactByEmailOrPhoneNumber service', err);
+        throw err;
+    }
+};
+
+export const insertNewContactInDB = async (data: object) => {
+    try {
+        const { data: Contact } = await supabase.from('Contact').insert(data).select();
+        return Contact;
+    } catch (err) {
+        console.log('Error in insertNewContact service', err);
         throw err;
     }
 };
